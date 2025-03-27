40ee0be7c7a498abb29152f93eca39a255aeafa1
Robert Kaussow <mail@thegeeklab.de>
2025-03-27 21:05:23 +0100

    Explain usage of chmod more detailed

--- plugin.go
+++ plugin.go
@@ -35,7 +35,10 @@ func (p Plugin) Exec() error {
 			return err
 		}
 
-		// Ensure correct mode if path already existed
+		// Ensure correct mode for path. This is also required even if the path does not exist
+		// and was created by os.MkdirAll. This is because the permissions set by os.MkdirAll
+		// are filtered by the system umask (usually 0022). As we need an extra chmod anyway,
+		// we don't mess around with the system umask.
 		os.Chmod(p.Pipeline.Path, 0o777)
 	}
 
