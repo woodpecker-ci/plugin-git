f40f5e9f7cf367b52592640f05d4cbe5d59e5932
qwerty287 <qwerty287@posteo.de>
2025-05-29 17:34:09 +0300

    Fix submodule override flag

--- plugin.go
+++ plugin.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -109,7 +110,12 @@ func (p Plugin) Exec() error {
 		cmds = append(cmds, checkoutSha(p.Pipeline.Commit))
 	}
 
-	for name, submoduleUrl := range p.Config.Submodules {
+	var submoduleOverrides map[string]string
+	err = json.Unmarshal([]byte(p.Config.Submodules), &submoduleOverrides)
+	if err != nil {
+		return fmt.Errorf("could not parse submodule_override map: %v", err)
+	}
+	for name, submoduleUrl := range submoduleOverrides {
 		cmds = append(cmds, remapSubmodule(name, submoduleUrl))
 	}
 
